c52b3225d4f4302db9ffa439bc7f80f367ec1459
Gift Brightson <42843422+gladiatorgiftson@users.noreply.github.com>
2022-05-21 22:57:40 +0530

    Added json config

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const port = process.env.PORT || 8080;
 
 // sendFile will go here
 app.get('/', function(req, res) {
-    // res.sendFile(path.join(__dirname, 'index.html'));
     res.sendFile("imdb.html", { root: __dirname })
   });
   
